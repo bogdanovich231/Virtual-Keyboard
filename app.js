34a47b7c92299b68564ea84f192fd24335535ec2
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-05-02 01:38:09 +0200

    feat: adding a mouse event

--- app.js
+++ app.js
@@ -175,6 +175,7 @@ keyboardArea.addEventListener('keyup', function (e) {
 keyboardLine.querySelectorAll('.keyboard_key').forEach(function (key) {
     key.addEventListener('mousedown', function () {
         key.classList.add('active');
+        keyboardArea.value += key.textContent;
     });
 
     key.addEventListener('mouseup', function () {
